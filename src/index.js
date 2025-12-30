3b9a180d738d31ac112c27cfc8ac65e251ec30af
Sarthak Singh Chaudhary <Sarthaksinghchaudhary0@gmail.com>
2025-12-30 12:42:36 +0530

    connected to mongo atlas

---
+++ src/index.js
@@ -0,0 +1,30 @@
+// Approach 1:
+
+// import mongoose from "mongoose";
+// import { DB_NAME } from "./constants.js";
+// import express from "express";
+// const app = express();
+
+// (async() =>{
+    //     try {
+        //         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        //         app.on("Error",error);
+        //         throw error;
+        //         app.listen(process.env.PORT, () => {
+//             console.log(`Server is running on port ${process.env.PORT}`);
+//         });
+//     } catch (error) {
+    //         console.error("Error connecting to the database:", error);
+    //         throw error;
+    //     }
+    // })()
+    
+// Bettter Approach - 2: Professional
+// require('dotenv').config();
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+dotenv.config({
+    path: "./.env"
+});
+    
+connectDB();
