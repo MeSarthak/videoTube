dc7ef9f4874a80ed486158aefc3a6ac11c53aad8
Sarthak Singh Chaudhary <Sarthaksinghchaudhary0@gmail.com>
2026-01-09 13:55:22 +0530

    login and logout functionality added

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,8 +1,22 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
-import { User } from "../models/User.js";
+import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
+import { set } from "mongoose";
+
+const generateAccessAndRefreshToken = async (userId) => {
+  try {
+    const user = await User.findById(userId);
+    const accessToken = user.generateAccessToken();
+    const refreshToken = user.generateRefreshToken();
+    user.refreshToken = refreshToken;
+    await user.save({ validateBeforeSave: false });
+    return { accessToken, refreshToken };
+  } catch (err) {
+    throw new ApiError(500, "Token generation failed");
+  }
+};
 
 const registerUser = asyncHandler(async (req, res) => {
   // get user details from frontend
@@ -31,7 +45,7 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new ApiError(400, "Password is required");
   }
 
-  const existedUser = User.findOne({ $or: [{ email }, { username }] });
+  const existedUser = await User.findOne({ $or: [{ email }, { username }] });
 
   if (existedUser) {
     throw new ApiError(409, "User already exists with this email or username");
@@ -67,7 +81,94 @@ const registerUser = asyncHandler(async (req, res) => {
   if (!createdUser) {
     throw new ApiError(500, "User creation failed");
   }
-  return res.status(201).json(new ApiResponse(201, createdUser, "User created successfully"));
+  return res
+    .status(201)
+    .json(new ApiResponse(201, createdUser, "User created successfully"));
 });
 
-export { registerUser };
+const loginUser = asyncHandler(async (req, res) => {
+  // get login details from frontend
+  // validation of login details - not empty
+  // check if user exists in DB: using username or email
+  // compare password
+  // generate access token and refresh token
+  // save refresh token in DB
+  // return response to frontend
+  const { email, username, password } = req.body;
+  if (!email && !username) {
+    throw new ApiError(400, "Email or Username is required");
+  }
+
+  const user = await User.findOne({
+    $or: [{ username }, { email }],
+  });
+
+  if (!user) {
+    throw new ApiError(404, "User not found");
+  }
+
+  const isPasswordValid = await user.isPasswordCorrect(password);
+  if (!isPasswordValid) {
+    throw new ApiError(401, "Invalid password");
+  }
+  const { accessToken, refreshToken } = await generateAccessAndRefreshToken(
+    user._id
+  );
+
+  const loggedInUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+
+  return res
+    .status(200)
+    .cookie("refreshToken", refreshToken, options)
+    .cookie("accessToken", accessToken, options)
+    .json(
+      new ApiResponse(
+        200,
+        {
+          user: loggedInUser,
+          accessToken,
+          refreshToken,
+        },
+        "User logged in successfully"
+      )
+    );
+});
+
+const logoutUser = asyncHandler(async (req, res) => {
+  await User.findByIdAndUpdate(
+    req.user._id,
+    {
+      $set:{
+        refreshToken: undefined
+      }
+    },
+    {
+      new: true
+    }
+  );
+  const options = {
+    httpOnly: true,
+    secure: true
+  };
+  return res.
+  status(200).
+  clearCookie("refreshToken", options).
+  clearCookie("accessToken", options).
+  json(
+    new ApiResponse(
+      200,
+      null,
+      "User logged out successfully"
+    )
+  );
+});
+
+
+export { registerUser, loginUser, logoutUser };
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,8 @@
 import {Router} from 'express';
 import { registerUser } from '../controllers/user.controller.js';
 import {upload} from '../middlewares/multer.middleware.js';
+import { loginUser, logoutUser } from '../controllers/user.controller.js';
+import { verifyJWT } from '../middlewares/auth.middleware.js';
 
 const router = Router();
 
@@ -8,5 +10,10 @@ router.route('/register').post(upload.fields([
     {name: 'avatar', maxCount: 1},
     {name: 'coverImage', maxCount: 1}
 ]) ,registerUser);
+
+router.route('/login').post(loginUser);
+//secure route
+router.route('/logout').post(verifyJWT, logoutUser);
+
 
 export {router};
