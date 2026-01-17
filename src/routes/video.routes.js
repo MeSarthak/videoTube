83544e37eb67530d0f2bac21989bccf54ccb4672
Sarthak Singh Chaudhary <Sarthaksinghchaudhary0@gmail.com>
2026-01-17 09:32:20 +0530

    creared for frontend

--- src/routes/video.routes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { uploadHLSVideo } from "../controllers/video.controller.js";
-import { upload } from "../middlewares/memoryMulter.middleware.js";
-const router = express.Router();
-
-router.post("/upload-abr", verifyJWT, upload.single("video"), uploadHLSVideo);
-
-export { router as videoRouter };
