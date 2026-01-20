3957952c5a262996070257a900a2bd13678aab05
Sarthak Singh Chaudhary <Sarthaksinghchaudhary0@gmail.com>
2026-01-20 18:26:20 +0530

    feat: Add health-check endpoint to verify API status

--- src/app.js
+++ src/app.js
@@ -21,7 +21,16 @@ app.use(cookieParser());
 //import routes
 import { userRouter  } from "./routes/user.routes.js";
 import { videoRouter  } from "./routes/video.routes.js";
+
 //use routes
+
+app.use("/health-check", (req, res) => {
+    res.status(200).json({
+        statusCode: 200,
+        message: "API is running...",
+        success: true
+    });
+});
 app.use("/api/v1/users", userRouter); //http://localhost:5000/api/v1/users/....
 app.use("/api/v1/videos", videoRouter); //http://localhost:5000/api/v1/videos/....
 
